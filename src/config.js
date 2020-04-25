ee6a15ec43ffc68b434450575605d36d0653affb
Moritz Marquardt <git@mo-mar.de>
2020-04-25 10:31:14 +0200

    Fix config (by using peers.json the correct way)

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 // See https://github.com/holtwick/peer2school-server
 export const SIGNAL_SERVER_URL = "ws" + window.location.protocol.substr(4) + "//" + window.location.host + window.location.pathname.replace(/[^\/]+$/, "")
 
+var request = new XMLHttpRequest();
+request.open('GET', '/peers.json', false);  // get peers synchronously
+request.send(null);
+
 // See https://github.com/feross/simple-peer#peer--new-peeropts
-// See https://jsxc.readthedocs.io/en/latest/howtos/webrtc.html#configure-stun-turn-server-in-jsxc
-export const ICE_CONFIG = {
-  url: './peers.json',
-  withCredentials: false,
-}
+export const ICE_CONFIG = JSON.parse(request.responseText);
